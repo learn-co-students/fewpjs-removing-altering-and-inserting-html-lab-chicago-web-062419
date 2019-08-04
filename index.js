f7e0ca45eda94dd008ba3d9930a04f49ff734a55
github username <github email address>
2019-08-04 16:45:44 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+
+
 // Write your code here!
+
+document.querySelector("main#main").remove();
+
+let newHeader = document.createElement('H1');
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML = 'GRACE is the champion';
