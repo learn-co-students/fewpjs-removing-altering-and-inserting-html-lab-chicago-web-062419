040bb996c035bf34b7b6de22ef0ce11292fb5769
Malisa Middlebrooks <middlebrooks314@gmail.com>
2019-08-04 03:41:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+
+const variable = "hello";
+
+
+document.querySelector("main#main").remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Malisa is the champion!";
